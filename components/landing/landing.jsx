7e7a89501bddc31a1d5d26632348156f597b48f4
Bonny Kato <6onnykato@gmail.com>
2022-09-15 14:59:14 +0300

    feat: update title

--- components/landing/landing.jsx
+++ components/landing/landing.jsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import {NetflixLogo} from "../svg";
 import Header from "./header";
 import BannerText from "./banner-text";
+import Head from "next/head";
 
 const Landing = () =>{
     return (
         <section className={'bg-black/60 relative overflow-hidden  h-[83.3vh]'}>
+            <Head>
+                <link rel="icon" href="/images/favicon.ico" type="image/ico"/>
+                <title>Wisiflix Movies, Always Get Latest Movies. </title>
+            </Head>
             <Image src={'/images/background.jpg'} alt={'background'}
                    className={' mix-blend-color-burn'} layout={'fill'} />
 
